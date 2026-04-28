7aa6c363ce2cc62b96129a9ecd30b509da195857
Drew Hanshaw <drew@andrewhanshaw.com>
2026-04-27 23:29:00 -0500

    Use sheet instead of fullscreencover

--- Demo/SwiftyCropDemo/ContentView.swift
+++ Demo/SwiftyCropDemo/ContentView.swift
@@ -246,7 +246,7 @@ struct ContentView: View {
       showImageCropper = false
     }
     #else
-    .fullScreenCover(isPresented: $showImageCropper) {
+    .sheet(isPresented: $showImageCropper) {
       imageCropperView
     }
     #endif
