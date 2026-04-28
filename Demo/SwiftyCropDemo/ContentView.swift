c0e86a0c6800805f26860c6700a0caa63341018b
Drew Hanshaw <drew@andrewhanshaw.com>
2026-04-27 23:54:26 -0500

    WIP comment stuff out

--- Demo/SwiftyCropDemo/ContentView.swift
+++ Demo/SwiftyCropDemo/ContentView.swift
@@ -281,12 +281,12 @@ struct ContentView: View {
         // Do something with the returned, cropped image
         self.selectedImage = croppedImage
       }
-      .clipped(antialiased: false)
-      #if os(macOS)
-      .frame(width: 600, height: 600)
-      #else
-      .ignoresSafeArea(edges: .all)
-      #endif
+      // .clipped(antialiased: false)
+      // #if os(macOS)
+      //   .frame(width: 600, height: 600)
+      // #else
+      //   .ignoresSafeArea(edges: .all)
+      // #endif
     }
   }
   
